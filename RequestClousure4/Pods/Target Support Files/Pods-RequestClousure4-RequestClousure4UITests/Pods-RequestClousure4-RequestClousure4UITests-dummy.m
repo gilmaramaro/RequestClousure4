4b8f9c455fcc21e6cb09b61dabc15eccde82cf12
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-27 18:59:18 -0300

    Projeto Criado

---
+++ RequestClousure4/Pods/Target Support Files/Pods-RequestClousure4-RequestClousure4UITests/Pods-RequestClousure4-RequestClousure4UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RequestClousure4_RequestClousure4UITests : NSObject
+@end
+@implementation PodsDummy_Pods_RequestClousure4_RequestClousure4UITests
+@end
